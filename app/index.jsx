fe202b0aab305cc423291d2eae935a77482f1920
xuerongNanopay <xuerong@nanopay.net>
2024-07-30 23:00:22 -0400

    add link

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack text-green-800">Foree!</Text>
-      {/* <Link href="/profile">Go to Profile</Link> */}
+      <StatusBar style="auto"/>
+      <Link href="/home">Go to home</Link>
     </View>
   )
 }
