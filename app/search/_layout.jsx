000b31187a508f26bc14b5c486d77a64ebfc6c5e
xuerongNanopay <xuerong@nanopay.net>
2024-07-30 22:54:48 -0400

    add default template for the files

---
+++ app/search/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchLayout = () => {
+  return (
+    <View>
+      <Text>SearchLayout</Text>
+    </View>
+  )
+}
+
+export default SearchLayout
